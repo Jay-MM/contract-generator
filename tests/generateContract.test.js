215c4041d29a8f182471ffa852085c5f6db3e1ea
Jesus M <marquez.jay444@gmail.com>
2024-01-31 19:22:55 -0600

    updates test suite

--- tests/generateContract.test.js
+++ tests/generateContract.test.js
@@ -35,42 +35,61 @@ describe("contract Object", () => {
     noticePeriod,
     revisionsCount,
     revisionDays,
+    noticePeriod,
   })
     console.log(contract)
 // Assert
     expect(typeof contract).toBe('string')
   })
   test("Has a date", () => {
-    
+// Act
+  const contract = generateContract({ date });
+// Assert
+  expect(contract).toContain(date);
   })
+// Act
   test("Has the developer's name", () => {
-    
+  const contract = generateContract({ developerName });
+// Assert
+  expect(contract).toContain(developerName);
   })
+// Act
   test("as the client's name", () => {
-    
+    const contract = generateContract({ clientName });
+// Assert
+    expect(contract).toContain(clientName);
   })
   test("The fee is greater than zero. If zero, has an explanation.", () => {
-    
+    const contract = generateContract({ clientName });
+// Assert
+    expect(contract).toContain(clientName);
   })
   test("Has payment terms(e.g: cash)", () => {
-    
+// Act
+// Assert
   })
   test("Has a completion date", () => {
-    
+// Act
+// Assert
   })
   test("the scope changes fee is a number greater than the original number", () => {
-    
+// Act
+// Assert
   })
   test("Confidentiality signature?", () => {
-    
+// Act
+// Assert
   })
   test("The contract has the amount of revisions the developer is willing to make", () => {
-    
+// Act
+// Assert
   })
   test(" The contract has the num of days the client can request revisions on the project.", () => {
-    
+// Act
+// Assert
   })
   test("has a notice period if either party wishes to terminate this agreement", () => {
-    
+// Act
+// Assert
   })
 })
